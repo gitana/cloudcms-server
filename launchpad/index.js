82fc570afcbb9c179efdc86b69591f801bdf6d1f
uzquiano <mgu1@cornell.edu>
2015-07-29 15:34:20 -0400

    Added environment-based way to set up launchpad Latest gitana driver Version bump

--- launchpad/index.js
+++ launchpad/index.js
@@ -6,6 +6,11 @@ var semver = require("semver");
 
 module.exports = function(options)
 {
+    // allow for override
+    if (process.env.CLOUDCMS_LAUNCHPAD_SETUP) {
+        options.setup = process.env.CLOUDCMS_LAUNCHPAD_SETUP;
+    }
+
     // default to single mode
     if (!options.setup) {
         options.setup = "single";
