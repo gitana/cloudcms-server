fca1b0c899db93917a2168cc522fb6b8df715a00
Harry Moore <harry@Harrys-MAC.home>
2016-03-28 10:15:27 -0400

    add configurable 404 handler

--- middleware/final/final.js
+++ middleware/final/final.js
@@ -22,12 +22,51 @@ exports = module.exports = function()
     var r = {};
 
     /**
-     * Hands back a 404.
-
+     * Hands back a 404 unless configured to redirect instead. ex.:
+     * 
+     *     "final": {
+     *         "enabled": true,
+     *         "redirectUrl": "/404.html",
+     *         "includeUrlHash": true,
+     *         "prefixList": ["/path1/", "/path2/"]
+     *     }
+     * 
      * @return {Function}
      */
     r.finalHandler = function()
     {
+        var config = {"enabled": false};
+        if(process.configuration.final)
+        {
+            config = process.configuration.final || {"enabled": false};
+        }
+        
+        if(config.enabled)
+        {
+            var redirectUrl = config.redirectUrl || "/index.html";
+            var prefixList = config.prefixList || ["/"];
+            var includeUrlHash = !!config.includeUrlHash;
+            
+            return util.createHandler("final", function(req, res, next, stores, cache, configuration) {
+                var targetUrl = redirectUrl;
+                if(includeUrlHash)
+                {
+                    targetUrl += "/#" + req.path;
+                }
+                
+                for(var i=0; i<prefixList.length; i++)
+                {
+                    if(req.path.startsWith(prefixList[i]))
+                    {
+                        res.redirect(targetUrl);
+                        return;
+                    }
+                }
+
+                util.status(res, 404).end();
+            });
+        }
+        
         return util.createHandler("final", function(req, res, next, stores, cache, configuration) {
             util.status(res, 404).end();
         });
